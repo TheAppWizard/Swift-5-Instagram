f0f1a2d7096e827fe5eceaa4518e801fab00389d
Shreyas Bhike <70090469+TheAppWizard@users.noreply.github.com>
2020-08-25 11:32:54 +0530

    Add files via upload

---
+++ Swift 5 Constants.playground/Contents.swift
@@ -0,0 +1,28 @@
+//Swift 5
+
+//Declaring Constants
+//Declaring a constant using the let keyword
+let double:Double = 2.0
+//double = 3.0 //Error : You can't reassign a let
+let inferredDouble = 2.0//Inferred as a Double
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
