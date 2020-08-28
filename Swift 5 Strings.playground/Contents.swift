3082167264f3a9d264fa5c86c3ee783097598a7c
Shreyas Bhike <70090469+TheAppWizard@users.noreply.github.com>
2020-08-28 13:05:20 +0530

    Add files via upload

---
+++ Swift 5 Strings.playground/Contents.swift
@@ -0,0 +1,41 @@
+//Swift 5
+
+//Strings
+//Using a string literal as an initial value
+let helloWorld = "Hello Swift"
+//Using a multiline string
+let helloWorldCode = """
+I'm the App Wizard
+"""
+//Empty String
+let emptyString = ""
+//Initializer Syntax
+let anotherEmptyString = String()
+
+
+//Mutating a String
+var mutableString = "Swift"
+mutableString += "Coding is Awesome"
+//String interpolation
+//Interpolating a Double
+let doubleValue = 0.5
+print("The Values is \(doubleValue)")
+//Interpolating a string
+print("This is my opinion :\(mutableString)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
